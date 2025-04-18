0c00e5485b2c5886780e968a9a60f2f1a2706c6e
flunkyventures <flunkyventures@gmail.com>
2025-04-18 23:29:52 +0300

    removed the main content before the footer

--- app/page.tsx
+++ app/page.tsx
@@ -17,44 +17,6 @@ export default function Home() {
           {/*<Hero />*/}
         </section>
         
-        {/* Main content */}
-        <main className="flex flex-col gap-[32px] items-center sm:items-start p-8 sm:p-20">
-          <Image
-            src="/flunky logo.png"
-            alt="Flunky Ventures logo"
-            width={180}
-            height={38}
-            priority
-          />
-          <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-            <li className="mb-2 tracking-[-.01em]">
-              This website is{" "}
-              <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-                under construction
-              </code>
-              .
-            </li>
-            <li className="tracking-[-.01em]">
-              Thank you for being patient :)
-            </li>
-          </ol>
-
-          <div className="flex gap-4 items-center flex-col sm:flex-row">
-            <a
-              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-              href=""
-              rel="noopener noreferrer"
-            >
-              <Image
-                src="/flunky logo.png"
-                alt="FlunkyVentures logomark"
-                width={20}
-                height={20}
-              />
-              Check again later
-            </a>
-          </div>
-        </main>
         <Footer />
       </div>
     </div>
